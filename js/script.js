f2b9d6d8e731cd3b13c39c863baee9ea7441247f
hanifahnurz <hanifahnurzh9@gmail.com>
2024-09-14 00:32:16 +0700

    add belah ketupat

--- js/script.js
+++ js/script.js
@@ -48,3 +48,34 @@ function hitungKelilingPersegi(){
     document.getElementById('keliling-persegi').value=kelilingPersegi;
     
 }
+
+function hitungLuasPersegiPanjang(){
+    panjang= document.getElementById('persegi-panjang').value;
+    lebar= document.getElementById('persegi-lebar').value;
+    luasPersegiPanjang= panjang*lebar;
+    document.getElementById('luas-persegi-panjang').value=luasPersegiPanjang;
+    
+}
+
+function hitungKelilingPersegiPanjang(){
+    panjang= document.getElementById('persegik-panjang').value;
+    lebar= document.getElementById('persegik-lebar').value;
+    kelilingPersegiPanjang= (parseInt(panjang)*2) + (parseInt(lebar)*2) ;
+    document.getElementById('keliling-persegi-panjang').value=kelilingPersegiPanjang;
+    
+}
+
+function hitungLuasBelah(){
+    d1= document.getElementById('d1').value;
+    d2= document.getElementById('d2').value;
+    luas= 0.5*d1*d2;
+    document.getElementById('luas-belah-ketupat').value=luas;
+
+}
+
+function hitungKelilingBelah(){
+    sisiBelah= document.getElementById('sisik-belah').value;
+    kelilingBelah= sisiBelah*4;
+    document.getElementById('keliling-belah-ketupat').value=kelilingBelah;
+    
+}
